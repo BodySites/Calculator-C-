973eab8d5ce52e670f50a12564703df38aaa2b0d
BodySites <100404530+BodySites@users.noreply.github.com>
2022-06-21 19:42:45 +0600

    operations

--- Calculator/Form1.cs
+++ Calculator/Form1.cs
@@ -12,9 +12,72 @@ namespace Calculator
 {
     public partial class Form1 : Form
     {
+ 
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void textBox3_TextChanged(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            double firstArg = Convert.ToDouble(textBox1.Text);
+            double secondArg = Convert.ToDouble(textBox3.Text);
+            double result = firstArg + secondArg;
+            textBox2.Text = result.ToString();
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            double firstArg = Convert.ToDouble(textBox1.Text);
+            double secondArg = Convert.ToDouble(textBox3.Text);
+            double result = firstArg - secondArg;
+            textBox2.Text = result.ToString();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            double firstArg = Convert.ToDouble(textBox1.Text);
+            double secondArg = Convert.ToDouble(textBox3.Text);
+            double result = firstArg * secondArg;
+            textBox2.Text = result.ToString();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            double firstArg = Convert.ToDouble(textBox1.Text);
+            double secondArg = Convert.ToDouble(textBox3.Text);
+            double result = firstArg / secondArg;
+            textBox2.Text = result.ToString();
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+            
         }
     }
 }
