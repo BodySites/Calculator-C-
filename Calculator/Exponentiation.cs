ca1b2e388ce8ceeb857e98ef30724b68878d9b2f
RomanDeva <107912840+RomanDeva@users.noreply.github.com>
2022-06-24 10:58:12 +0600

    Create Exponentiation.cs

---
+++ Calculator/Exponentiation.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculator
+{
+    internal class MultiplyCalculator : ITwoArgumentsCalculator
+    {
+        public double Calculate(double firstValue, double secondValue)
+        {
+            return (float)Math.Pow(firstValue , secondValue);
+        }
+    }
+}
