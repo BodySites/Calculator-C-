322c98f6dcf8f95e51bc83edaca6c357b6970fdc
RomanDeva <107912840+RomanDeva@users.noreply.github.com>
2022-06-24 11:17:17 +0600

    Create Remains.cs

---
+++ Calculator/Remains.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculator
+{
+    internal class SubtractionCalculator : ITwoArgumentsCalculator
+    {
+        public double Calculate(double firstValue, double secondValue)
+        {
+            return firstValue % secondValue;
+        }
+    }
+}
